10d44557617c0f954be8076df9d3ef85b307a119
Tai Nguyen <tainguyen880s@gmail.com>
2023-11-04 12:59:46 +0700

    v-12 dark light mode base

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
+import { Button, Typography } from "@mui/material"
+import { useState } from "react"
 
-import { Button } from '@mui/material'
-import { green } from '@mui/material/colors'
-import { Home } from '@mui/icons-material'
+// toggle dark ? light
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme,
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
-
+  const [changeColor, setChangeColor] = useState(false)
   return (
     <>
       <div>
-        <Button variant="contained">Hello World</Button>
-        <Button variant='outlined'>Hello World</Button>
+        <ModeToggle />
+        <Button variant="outlined"
+          onClick={() => setChangeColor(!changeColor)}
+        >
+          Change Color
+        </Button>
 
-        <Home sx={{ color: green[600] }}/>
+
+        <Typography variant="body2" color={changeColor ? 'text.secondary' : 'text.primary'}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nemo
+          sit incidunt debitis dicta ad molestias eum delectus! Corrupti
+          voluptatem velit consequatur reprehenderit, voluptatibus accusamus
+          nostrum vel doloremque numquam atque?
+        </Typography>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import CssBaseline from '@mui/material/CssBaseline' // this is a HOC
-
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+import theme from "./theme.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline>
-      <App />
-    </CssBaseline>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </CssVarsProvider>
   </React.StrictMode>
 )
