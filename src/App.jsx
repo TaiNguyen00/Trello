dc71cd598eb3fafe7cdf058c5a9fd1ca8a6bba1a
Tai Nguyen <tainguyen880s@gmail.com>
2024-01-02 17:11:49 +0700

    refactor project struc

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,11 @@
-import { Button, Typography } from "@mui/material"
-import { useState } from "react"
-
-// toggle dark ? light
-import {
-  Experimental_CssVarsProvider as CssVarsProvider,
-  experimental_extendTheme as extendTheme,
-  useColorScheme,
-} from '@mui/material/styles'
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
+import Board from "./pages/Boards/_id"
 
 function App() {
-  const [changeColor, setChangeColor] = useState(false)
   return (
+    // sử dụng disable và maxWitdh để k bị giới hạn, sử dụng height vh sẽ k bị scroll, dùng vw sẽ có scroll
     <>
-      <div>
-        <ModeToggle />
-        <Button variant="outlined"
-          onClick={() => setChangeColor(!changeColor)}
-        >
-          Change Color
-        </Button>
-
-
-        <Typography variant="body2" color={changeColor ? 'text.secondary' : 'text.primary'}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nemo
-          sit incidunt debitis dicta ad molestias eum delectus! Corrupti
-          voluptatem velit consequatur reprehenderit, voluptatibus accusamus
-          nostrum vel doloremque numquam atque?
-        </Typography>
-      </div>
+      {/* React Router dom / boards/ {board_id} */}
+      <Board />
     </>
   )
 }
